f33cf2f524df1b6e4a3a939cd5531f140f0c9665
ayush <ayushmehuljoshi@gmail.com>
2025-04-05 22:11:16 -0700

    Built a python package with one simple function

---
+++ src/toy_pkg/say_hello.py
@@ -0,0 +1,5 @@
+def say_hello(name: str) -> str:
+    """
+    Return a greeting for the given name.
+    """
+    return f"Hello, {name}!"
---
+++ tests/test_toy_pkg.py
@@ -0,0 +1,4 @@
+from toy_pkg import say_hello
+
+def test_say_hello():
+    assert say_hello("Ayush") == "Hello, Ayush!"
